baca03b16d4e6354e12fadabebb6bc4372fe1fc6
Supcomfreak <lunarmoon56@gmail.com>
2018-06-22 07:49:14 +0000

    Add files via upload
    Add initial Gaydar.py

---
+++ Gaydar.py
@@ -0,0 +1,11 @@
+#How gay
+
+gayness = 0 #By default everybody is straight
+
+gender = input("What is your gender? \n Male: 0\n Female: 1\n") #Identify the gender of the person performing the action
+type(gender)
+
+actors = imput("How many other people are included in the scenario?") #Identify number of potential gay actors
+type(actors)
+
+
